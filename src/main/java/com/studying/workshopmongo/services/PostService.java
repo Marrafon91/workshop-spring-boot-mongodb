d08b877837bd9d75b945f2c5dcbac309e92ec3aa
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-01 09:21:15 -0300

    Consulta com varios criterios

--- src/main/java/com/studying/workshopmongo/services/PostService.java
+++ src/main/java/com/studying/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.studying.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,10 @@ public class PostService {
 
 	public List<Post> findByTitle(String Text) {
 		return repo.searchTitle(Text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
